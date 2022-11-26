37988f8db4bc8a376dd24f7c4e63a110d42e9ccc
jaya4ever <111536082+jaya4ever@users.noreply.github.com>
2022-11-26 07:21:50 -0600

    created function that returns the license section of readme

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -8,18 +8,61 @@ function renderLicenseLink(license) {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  
+  if (license === 'MIT' ){
+    return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]"
+  } else if(license === 'GNU'){
+     return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)]"
+  } else if (license === 'Apache'){
+    return "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)]"
+  }else{
+    return ""
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
   ## License
-  [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]
+${renderLicenseSection({license})}
 
   ## Description
   ${data.description}
 
+  
   ## Table of contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Contribution](#contribution)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  * [License](#license)
+  
+
+  ## Installation
+
+
+  ## Usage
+
+
+
+  ## Contribution 
+
+
+  
+  
+  ## Tests
+
+
+
+  
+  ## Questions
+
+
+
+
+  ## License
 
 
 
